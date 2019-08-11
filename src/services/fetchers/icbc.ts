5521398c22eac92b5487e579fa383d096aad3cb4
Edu Depetris <edu.depetris.00@gmail.com>
2019-08-10 22:53:40 -0300

    Add ICBC 🏦

---
+++ src/services/fetchers/icbc.ts
@@ -0,0 +1,35 @@
+import BankOptions from '../bankInterfaces'
+import axios from 'axios'
+
+class ICBC {
+  readonly url: string;
+
+  constructor () {
+    this.url = "http://bit.ly/2YHE8Ra";
+  }
+
+  run () : Promise<BankOptions> {
+    return axios.get(this.url)
+    .then(response => {
+      const result = response.data;
+
+      let buy = parseFloat(result['valorCompra'].replace(',', '.'));
+      let sell = parseFloat(result['valorVenta'].replace(',', '.'));
+
+      return {
+        name: 'Icbc',
+        buy: buy,
+        sell: sell
+      }
+    })
+    .catch(err => {
+      return {
+        name: 'Icbc',
+        buy: 0,
+        sell: 0
+      }
+    })
+  }
+}
+
+export default ICBC
---
+++ test/services/fetchers/icbc.spec.ts
@@ -0,0 +1,35 @@
+import ICBC from '../../../src/services/fetchers/icbc';
+import BankOptions from '../../../src/services/bankInterfaces';
+import { expect } from 'chai';
+import * as nock from 'nock';
+import * as fs from 'fs';
+
+describe('fetchers/icbc', () => {
+  it ('extracts the dolar', async () => {
+    const fetcher = new ICBC();
+    
+    const json = fs.readFileSync('test/services/data/icbc_10_08_2019.json');
+    
+    nock(fetcher.url)
+    .get('')
+    .reply(200, json, { 'Content-Type': 'application/json' });
+    
+    const result : BankOptions = await fetcher.run();
+    expect(result.name).to.equal('Icbc');
+    expect(result.buy).to.equal(44.2);
+    expect(result.sell).to.equal(46.6);
+  })
+
+  it ('returns empty values if request get 404', async () => {
+    const fetcher = new ICBC();
+
+    nock(fetcher.url)
+    .get('')
+    .reply(404, {}, { 'Content-Type': 'application/json' });
+
+    const result : BankOptions = await fetcher.run();
+    expect(result.name).to.equal('Icbc');
+    expect(result.buy).to.equal(0);
+    expect(result.sell).to.equal(0);
+  })
+})
